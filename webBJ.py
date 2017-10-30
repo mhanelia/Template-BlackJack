d806416b73468942877b12161e5435c734938191
mhanelia <murilo.aippe@hotmail.com>
2017-10-30 16:53:55 -0200

    update

--- webBJ.py
+++ webBJ.py
@@ -6,32 +6,25 @@ b = BlackJack()
 b.shuffle()
 pc_hand = b.two_first_cards()
 player_hand = b.two_first_cards()
-
 @app.route('/')
-def layout():
-    return render_template('layout.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
 
-@app.route('/start')
-def start():
-
-    return render_template('jogar.html')
-
 @app.route('/jogar', methods = ['GET', 'POST'])
 def jogar():
+
     if request.method == 'POST':
         newHand = player_hand
         newHand = b.hit(newHand)
-
+    while (b.get_point(pc_hand) <= 16):
+            npc_hand = b.hit(pc_hand)
     return render_template('jogar.html',mJogador = player_hand, pJogador = b.get_point(player_hand), mPC = pc_hand[0])
 
 @app.route('/resultado', methods = ['GET', 'POST'])
 def resultado():
-    if request.method == 'GET':
 
         return render_template('resultado.html', resultado = b.bet(b.get_point(pc_hand), b.get_point(player_hand)))
-
-    return render_template('resultado.html')
